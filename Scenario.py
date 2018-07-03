072c208da337f9e83ee5ce5c58e4721e4757d23f
spannercitest <40029474+spannercitest@users.noreply.github.com>
2018-07-03 10:47:00 +0300

    New scenario

--- Scenario.py
+++ Scenario.py
@@ -1,17 +1,8 @@
 from Testboard import Testboard
 from Ifttt import Ifttt
 import time
-import json
 
 BINARY_FROM = "SPANNER"
-TESTBOARD_ID = "340040000f51353532343635"
-IFTTT_ACCESS_TOKEN = "54c8df8cb04da38a34e26ec6da046abf92182de4"
-
-testboard = Testboard(TESTBOARD_ID)
-ifttt = Ifttt(IFTTT_ACCESS_TOKEN)
-
-RELAY_PIN = "D7"
-
 TESTBOARD_ID = "340040000f51353532343635"
 IFTTT_ACCESS_TOKEN = "54c8df8cb04da38a34e26ec6da046abf92182de4"
 
@@ -22,39 +13,39 @@ ifttt = Ifttt(IFTTT_ACCESS_TOKEN)
 RELAY_PIN = "D7"
 
 # Cloud Functionality
-
 def validate_network_cmd_on():
-    ifttt.buttonOn()
+#    ifttt.buttonOn()
 
+    testboard.digitalWrite(RELAY_PIN, 'HIGH')
     time.sleep(2)
 
     value = testboard.digitalRead(RELAY_PIN)
-    print (value)
-    if value['return_value'] ==1:
+    if testboard.spanner_assertTrue(value) == 1:
         return 0 #Success
     else:
         return 1 #Failure
 
     # check PIN state
     value = testboard.digitalRead(RELAY_PIN)
-    if (testboard.assert_spanner(value) == 1):
+    if (testboard.spanner_assertTrue(value) == 1):
         return 0 # Success
     else:
         return 1 # Failure
 
 # Cloud Functionality
 def validate_network_cmd_off():
-    ifttt.buttonOff()
+#    ifttt.buttonOff()
 
+    testboard.digitalWrite(RELAY_PIN, 'LOW')
     time.sleep(2)
 
     value = testboard.digitalRead(RELAY_PIN)
-    print (value)
-    if value['return_value'] ==0:
+    if (testboard.spanner_assertTrue(value) == 0):
         return 0
+
     # check PIN state
     value = testboard.digitalRead(RELAY_PIN)
-    if (testboard.assert_spanner(value) == 0):
+    if (testboard.spanner_assertTrue(value) == 0):
         return 0 # Success
     else:
         return 1 # Failure
