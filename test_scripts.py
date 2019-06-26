698932229cb0bbb99345086acb5c11c7d78d5a28
Vasilis Georgitzikis <tzikis@gmail.com>
2019-06-26 16:34:58 +0300

    Update test_scripts.py

--- test_scripts.py
+++ test_scripts.py
@@ -2,21 +2,29 @@ from SpannerTestboard import SpannerTestboard
 from Ifttt import Ifttt
 import time
 import json
+import requests
+import pytest
 
 
-TESTBOARD_ID = "340040000f51353532343635"
+TESTBOARD_NAME = "Testboard1"
 IFTTT_ACCESS_TOKEN = "54c8df8cb04da38a34e26ec6da046abf92182de4"
 
-testboard = Testboard(TESTBOARD_ID)
-ifttt = Ifttt(IFTTT_ACCESS_TOKEN)
-
+testboard = SpannerTestboard(TESTBOARD_NAME)
 # D7 -> Relay PIN
 RELAY_PIN = "D7"
 
-# Cloud Functionality
-def validate_network_cmd_on():
-    ifttt.buttonOn()
+def set_request(endpoint):
+    headers = {"Content-Type": "application/json"}
+    r = requests.post(endpoint, data = '', headers=headers)
+    return r.text
 
+def send_command(command):
+    endpoint = 'https://maker.ifttt.com/trigger/'+command+'/with/key/'+ifttt_key
+    return set_request(endpoint)
+
+# Cloud Functionality
+def test_validate_network_cmd_on():
+    send_command("turn_on")
     time.sleep(2)
 
     # check PIN state
@@ -27,9 +35,8 @@ def validate_network_cmd_on():
         return 1 # Failure
 
 # Cloud Functionality
-def validate_network_cmd_off():
-    ifttt.buttonOff()
-
+def test_validate_network_cmd_off():
+    send_command("turn_off")
     time.sleep(2)
 
     # check PIN state
@@ -38,11 +45,3 @@ def validate_network_cmd_off():
         return 0 # Success
     else:
         return 1 # Failure
-
-if __name__ == "__main__":
-
-    run_test(validate_network_cmd_on())
-
-    time.sleep(2)
-
-    run_test(validate_network_cmd_off())
